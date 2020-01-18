2df6f871fb7f7a89f324793a013c92cc4ae3c935
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 17:41:31 -0700

    Added routes to post-router

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,23 +5,75 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+/*
+router.get('/', async (req, res, next) => {
+    try {
+       //res.json(await db.select("*").from("posts")); 
+       //res.json(await db("posts"));
+       res.json(await db("posts").select());
+    } catch (err) {
+        next(err);
+    }
+});
+*/
+
+router.get('/', (req, res, next) => {
+    db("posts")
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+       //res.json(await db.select("*").from("posts").where("id", req.params.id)); 
+       //res.json(await db("posts").where("id", req.params.id).select());  
+       //res.json(await db("posts").where("id", req.params.id));
+       //res.json(await db("posts").where({ id:req.params.id })); //returns an array
+       res.json(await db("posts").where({ id:req.params.id }).first());  //returns an object
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }; 
+        const [id] = await db("posts").insert(payload);
+        res.json(await db("posts").where({ id: id }).first());
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        };
+        const count = await db("posts").where({ id: req.params.id}).update(payload);
+        res.json({count: count, post: await db("posts").where({ id: req.params.id }).first()});  
+    } catch (err) {
+        next(err);
+    }
 
 });
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.post('/', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res, next) => {
+    try {
+         const postToDelete = await db("posts").where({id: req.params.id}).first();
+         const numAffectedRows = await db("posts").where({id: req.params.id}).del();
+         res.json({numAffectedRows: numAffectedRows, deletedPost : postToDelete});
+    } catch (err) {
+        next(err);
+    }
 
 });
 
--- server.js
+++ server.js
@@ -12,4 +12,9 @@ server.get('/', (req, res) => {
   res.send('<h3>DB Helpers with knex</h3>');
 });
 
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({error: err, message: "Error caught in error handling middleware"});
+});
+
 module.exports = server;
